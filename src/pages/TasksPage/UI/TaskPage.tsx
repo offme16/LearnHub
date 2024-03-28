8195d32326ba89508c64a6abc74075a21a42c575
offme16 <offmemyg@gmail.com>
2024-03-28 22:16:35 +0300

    .

--- src/pages/TasksPage/UI/TaskPage.tsx
+++ src/pages/TasksPage/UI/TaskPage.tsx
@@ -3,29 +3,38 @@ import cls from './TaskPage.module.scss';
 import { useSelector } from 'react-redux';
 import { getTasks } from 'entities/Stack';
 import Button from 'shared/UI/Button/Button';
+import Slider from 'react-slick';
 
 const TaskPage = () => {
     const asa = useSelector(getTasks)
-    
+    const mas = [1, 2, 3, 4]; 
+    const settings = {
+        dots: false,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: false,
+      };
     return (
         <div>
             <Header />
-            <div className={cls.container}>
+             <Slider {...settings}>
+              <div className={cls.container}>
                 <div className={cls.container_description}>
-                        <h2>QUESTION</h2>
+                    <h2>QUESTION</h2>
+                    <p>fhsdjbuwdsyvcgbywevbwshdvbsdvhsdiov</p>
                 </div>
                 <form className={cls.container_form}>
-                <div className={cls.container_form__input}>
+                    <h2>Выберите один из вариантов ответа</h2>
+                {mas.map(e => <div className={cls.container_form__input} >
                         <input type='radio' id='radioInput' name='radioGroup'/>
                         <label htmlFor='radioInput'>Текст, связанный с инпутом</label>
-                    </div>
-                    <div className={cls.container_form__input}>
-                        <input type='radio' id='radioInpu' name='radioGroup'/>
-                        <label htmlFor='radioInpu'>Текст, связанный с инпутом</label>
-                    </div>
-                    <Button >Следующий</Button>
+                    </div>)}
                 </form>
             </div>
+             </Slider>
+                
         </div>
     )
 }
